548e1c75131412e176a63810862e1aa87f8193c8
Asger Song <202106630@post.au.dk>
2023-11-29 20:48:14 +0100

    Changed file names

--- bully-in-kubernetes/app.py
+++ bully-in-kubernetes/app.py
@@ -1,67 +1,99 @@
 import asyncio
-from aiohttp import web
+from aiohttp import web, ClientSession
 import os
 import socket
 import random
-import aiohttp
-import requests
 
 POD_IP = str(os.environ['POD_IP'])
 WEB_PORT = int(os.environ['WEB_PORT'])
 POD_ID = random.randint(0, 100)
+leader_id = None
 
 async def setup_k8s():
-    # If you need to do setup of Kubernetes, i.e. if using Kubernetes Python client
-	print("K8S setup completed")
- 
+    print("K8S setup completed")
+
+async def send_message(pod_ip, endpoint, data):
+    async with ClientSession() as session:
+        url = f'http://{pod_ip}:{WEB_PORT}{endpoint}'
+        try:
+            await session.post(url, json=data)
+        except Exception as e:
+            print(f"Error sending message to {pod_ip}: {e}")
+
 async def run_bully():
+    global leader_id
     while True:
         print("Running bully")
         await asyncio.sleep(5) # wait for everything to be up
-        
+
         # Get all pods doing bully
         ip_list = []
         print("Making a DNS lookup to service")
-        response = socket.getaddrinfo("bully-service",0,0,0,0)
+        response = socket.getaddrinfo("bully-service", 0, 0, 0, 0)
         print("Get response from DNS")
         for result in response:
             ip_list.append(result[-1][0])
         ip_list = list(set(ip_list))
-        
+
         # Remove own POD ip from the list of pods
         ip_list.remove(POD_IP)
-        print("Got %d other pod ip's" % (len(ip_list)))
-        
-        # Get ID's of other pods by sending a GET request to them
+        print(f"Got {len(ip_list)} other pod ip's")
+
+        # Get IDs of other pods by sending a GET request to them
         await asyncio.sleep(random.randint(1, 5))
         other_pods = dict()
-        for pod_ip in ip_list:
-            endpoint = '/pod_id'
-            url = 'http://' + str(pod_ip) + ':' + str(WEB_PORT) + endpoint
-            response = requests.get(url)
-            other_pods[str(pod_ip)] = response.json()
-            
-        # Other pods in network
-        print(other_pods)
-        
-        # Sleep a bit, then repeat
+        async with ClientSession() as session:
+            for pod_ip in ip_list:
+                url = f'http://{pod_ip}:{WEB_PORT}/pod_id'
+                try:
+                    async with session.get(url) as response:
+                        other_pods[pod_ip] = await response.json()
+                except Exception as e:
+                    print(f"Error getting pod ID from {pod_ip}: {e}")
+
+        # Check if there's need to initiate an election
+        if leader_id is None or leader_id not in other_pods.values():
+            higher_ids = [ip for ip, id in other_pods.items() if id > POD_ID]
+            if not higher_ids:  # No higher IDs, declare self as leader
+                leader_id = POD_ID
+                for pod_ip in ip_list:
+                    await send_message(pod_ip, '/receive_coordinator', {"leader_id": POD_ID})
+            else:
+                for pod_ip in higher_ids:
+                    await send_message(pod_ip, '/receive_election', {"origin_id": POD_ID})
+                    await asyncio.sleep(1)  # Wait for responses
+
         await asyncio.sleep(2)
-    
-#GET /pod_id
+
+# GET /pod_id
 async def pod_id(request):
-    return web.json_response(POD_ID)
+    return web.json_response({"id": POD_ID})
     
-#POST /receive_answer
+# POST /receive_answer
 async def receive_answer(request):
-    pass
+    global leader_id
+    data = await request.json()
+    responder_id = data.get("responder_id")
+    if responder_id > POD_ID:
+        leader_id = responder_id
+    return web.Response(text="Received answer")
 
-#POST /receive_election
+# POST /receive_election
 async def receive_election(request):
-    pass
+    data = await request.json()
+    origin_id = data.get("origin_id")
+    if POD_ID > origin_id:
+        # Send answer back to originator
+        await send_message(origin_id, '/receive_answer', {"responder_id": POD_ID})
+    # Start own election process
+    return web.Response(text="Election received")
 
-#POST /receive_coordinator
+# POST /receive_coordinator
 async def receive_coordinator(request):
-    pass
+    global leader_id
+    data = await request.json()
+    leader_id = data.get("leader_id")
+    return web.Response(text="New Coordinator Acknowledged")
 
 async def background_tasks(app):
     task = asyncio.create_task(run_bully())
